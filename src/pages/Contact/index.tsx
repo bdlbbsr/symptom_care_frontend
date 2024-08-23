fc1f9bc32576d5587dc10e740e45d76e790619ef
Biswajit Sahoo <bdlbbsr@gmail.com>
2024-08-23 11:58:11 +0530

    --

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -8,8 +8,27 @@ function Contact() {
       <BackButton />
       </div>
       <div>
-        <h1>Contact</h1>
-        <article>Contact text</article>
+      <main className="contentDiv scrollMiddle">
+       <>
+       <h1>Do you want to buy this script only  / with domain?</h1>
+
+       <p>Features:</p>
+       <ul>
+        <li>People / doctor can resgister and activate by secure email link</li>
+        <li>Admin can Enter new symptoms</li>
+        <li>User can post his symptom name with photo anonymously</li>
+        <li>Doctor can see the user's request and add description for that symptom name</li>
+        <li>When user search any symptom, nearby doctors displays in rightside</li>
+        <li>When user goto symptom details page, nearby doctors in same department displayin rightside</li>
+        <li>In doctor's page all doctors list showing, you can filter according departments</li>
+        <li>In department page all the department with small description listed there</li>
+       </ul>
+       </>
+
+       <p>If you wany any customization like new feature addition then let me know</p>
+       <p>Contact me at badalray@yahoo.com</p>
+
+    </main>
       </div>
     </div>
   )
