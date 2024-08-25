ebc6bb63cabd0b711dc7dddebd269eb80bb49899
Biswajit Sahoo <bdlbbsr@gmail.com>
2024-08-25 19:42:14 +0530

    --

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,37 +33,39 @@ const Home: FC = () => {
 
   return (
     <div className="searchDiv">
-      <div className="search-box">
-        <button
-          onClick={handleSearch}
-          className={`btn-search ${isFocused ? 'focusRemoved' : 'focusAdded'}`}
-          title="Search Symptoms"
-          aria-label="Search symptoms"
+    <div className="search-box">
+      <button
+        onClick={handleSearch}
+        className={`btn-search ${isFocused ? 'focusRemoved' : 'focusAdded'}`}
+        aria-label="Search for symptoms"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          className="bi bi-search iconClr"
+          viewBox="0 0 16 16"
+          role="img" // Ensure the icon is treated as an image
+          aria-labelledby="searchIconTitle" // Links title to SVG
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            className="bi bi-search iconClr"
-            viewBox="0 0 16 16"
-            aria-hidden="true"  // This hides the SVG from screen readers
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-          </svg>
-        </button>
-        <input
-          type="text"
-          className="input-search"
-          placeholder="Search symptom... for ex. pain"
-          onChange={handleChange}
-          onKeyDown={handleKeyPress}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          aria-label="Search for symptoms"
-        />
-      </div>
+          <title id="searchIconTitle">Search Icon</title> 
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+        </svg>
+      </button>
+      <input
+        type="text"
+        className="input-search"
+        placeholder="Search symptom... for ex. pain"
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        aria-label="Enter symptom keywords to search"
+      />
     </div>
+  </div>
+  
   );
 };
 
